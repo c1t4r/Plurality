06a04883ac5d240553b2c36b73761d0222c5ceb9
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-06-14 15:07:54 +0200

    make modules work

--- update_base_container.sh
+++ update_base_container.sh
@@ -45,7 +45,10 @@ IncludeCmd: yes
     echo "This is what happens when you run the container..."
 
 %environment
-    export PS1='\[\033[01;32m\]\u@\${SINGULARITY_CONTAINER}@\h\[\033[01;34m\] \w \\$\[\033[00m\] '
+module() { eval `/usr/bin/modulecmd sh $*`; }
+export MODULEPATH=/opt/bwhpc/ul/modulefiles:/opt/bwhpc/common/modulefiles
+export PSM_SHAREDCONTEXTS_MAX=6 # do NOT allocate all HCs, leave remaining HCs for other MPI apps running simultaneously
+export PS1="\[\033[01;32m\]\u@${SINGULARITY_CONTAINER}@\h\[\033[01;34m\] \w \$\[\033[00m\] "
 
 %post
     echo "Installing JUSTUS software package list"
