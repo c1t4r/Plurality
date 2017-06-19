7213bb14f47268070915f56c1a4ae4b1fe98c0b6
Stefan Kombrink <stefan.kombrink@uni-ulm.de>
2017-06-19 14:51:09 +0200

    import user env settings

--- update_base_container.sh
+++ update_base_container.sh
@@ -46,11 +46,17 @@ echo "This code is executed as default run script. For now invoke a bash shell..
 /bin/bash
 
 %environment
-# These environment settings are needed to make containers run JUSTUS software stack
+# These environment settings are needed to make containers run the JUSTUS software stack
 module() { eval `/usr/bin/modulecmd sh $*`; }
 export MODULEPATH=/opt/bwhpc/ul/modulefiles:/opt/bwhpc/common/modulefiles
 export PSM_SHAREDCONTEXTS_MAX=6 # do NOT allocate all HCs, leave remaining HCs for other MPI apps running simultaneously
 export PS1="\[\033[01;32m\]\u@${SINGULARITY_CONTAINER}@\h\[\033[01;34m\] \w \$\[\033[00m\] "
+# Now import user defined settings
+for script in /custom/userenv/*.sh; do
+    if [ -f "$script" ]; then
+        . $script
+    fi
+done
 
 %post
     echo "Installing JUSTUS software package list"
